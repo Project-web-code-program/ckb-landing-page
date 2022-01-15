a5d42c7d2da0c6bf13c3d8d68fbf9ac970f50d57
adiynsyah <adiyansyahdwiputra@gmail.com>
2022-01-15 21:05:03 +0700

    update lang ckb 30%

--- assets/js/language.js
+++ assets/js/language.js
@@ -2,11 +2,29 @@ const keyOfDict = 'i18n-key';
 const dictionary = {
   id: {
     'nav-home': 'Utama',
-    'nav-about': 'Tentang Kami'
+    'nav-about': 'Tentang Kami',
+    'nav-bidang-usaha': 'Bidang Usaha',
+    'nav-teknologi': 'Teknologi',
+    'nav-edukasi': 'Edukasi',
+    'nav-seni': 'Seni',
+    'nav-design': 'Desain',
+    'nav-tour&travel': 'Wisata & Perjalanan',
+    'nav-layanan': 'Layanan',
+    'nav-blog': 'Blog',
+    'nav-kontakkami': 'Kontak Kami'
   },
   en: {
     'nav-home': 'Home',
-    'nav-about': 'About Us'
+    'nav-about': 'About Us',
+    'nav-bidang-usaha': 'Business Fields',
+    'nav-teknologi': 'Technology',
+    'nav-edukasi': 'Education',
+    'nav-seni': 'Art',
+    'nav-design': 'Design',
+    'nav-tour&travel': 'Tour & Travel',
+    'nav-layanan': 'Service',
+    'nav-blog': 'Blog',
+    'nav-kontakkami': 'Contact Us'
   }
 };
 
